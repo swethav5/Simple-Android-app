a294807e9f3ac851526290edb7d5a321faa40a7c
swethav5 <swethav512@gmail.com>
2020-08-03 16:39:49 +0530

    places added

--- Mainactivity/app/src/main/java/com/example/mainactivity/Homeactivity.java
+++ Mainactivity/app/src/main/java/com/example/mainactivity/Homeactivity.java
@@ -5,19 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Homeactivity extends AppCompatActivity {
 
-    TextView llogout;
+    Button llogout;
+    Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeactivity);
-        llogout=(TextView)findViewById(R.id.logouttextview);
+        llogout=(Button) findViewById(R.id.logoutbutton);
+        sp = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(Homeactivity.this,R.array.places,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String place=adapterView.getItemAtPosition(i).toString();
+                if(place=="Hyderabad"){
+                    startActivity(new Intent(Homeactivity.this,place1.class));
+                }
+                else if(place=="Mumbai"){
+                    startActivity(new Intent(Homeactivity.this,place2.class));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         llogout.setOnClickListener(new View.OnClickListener() {
             @Override
